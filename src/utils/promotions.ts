e293685871b2da4fe7d4d1dbc7bf6e501cb643cd
Thomas de Lachaux <thomas.delachaux@gmail.com>
2019-11-21 16:15:15 +0100

    Updated promomtions for netgame

--- src/utils/promotions.ts
+++ src/utils/promotions.ts
@@ -1,40 +1,11 @@
 import { Promotion, Price } from '../types';
 import Item from '../models/item';
 
-// TODO: Attendre que Julo mette à jour les promos
 export const getPromotions = (): Array<Promotion> => [
-  {
-    formula: ['barre', 'canette'],
-    orgaPrice: 110,
-    price: 170,
-    name: 'Promo barre canette',
-    key: 'barre-canette',
-  },
-  {
-    formula: ['pizza', 'canette'],
-    orgaPrice: 400,
-    price: 500,
-    name: 'Promo pizza canette',
-    key: 'pizza-canette',
-  },
-  {
-    formula: ['crepe', 'canette'],
-    orgaPrice: 150,
-    price: 250,
-    name: 'Promo crêpe canette',
-    key: 'crepe-canette',
-  },
-  {
-    formula: ['croque', 'croque', 'croque', 'canette'],
-    orgaPrice: 250,
-    price: 450,
-    name: 'Promo 3 croques canette',
-    key: 'croque-canettes',
-  },
   {
     formula: ['croque', 'croque', 'croque'],
     orgaPrice: 200,
-    price: 400,
+    price: 200,
     name: 'Promo 3 croques',
     key: 'croque',
   },
