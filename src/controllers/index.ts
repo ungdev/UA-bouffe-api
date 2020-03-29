7ee635889cbe7fcce662129d2953bef2ac5ecdd5
Thomas de Lachaux <thomas.delachaux@gmail.com>
2020-03-29 22:11:52 +0200

    Add status page

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -5,10 +5,12 @@ import isAuth from '../middlewares/isAuth';
 import category from './category';
 import item from './item';
 import promotions from './promotions';
+import status from './status';
 
 const routes = () => {
   const router = Router();
 
+  router.get('/', status());
   router.use('/auth', auth());
   router.use('/orders', isAuth(), order());
   router.use('/categories', isAuth(), category());
---
+++ src/controllers/status.ts
@@ -0,0 +1,3 @@
+import { Request, Response } from 'express';
+
+export default () => (req: Request, res: Response) => res.status(200).send('API UP !').end();
